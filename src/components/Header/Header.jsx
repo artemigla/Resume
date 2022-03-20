dd1b2be10ea008c7c28cbe2f5dd2b61deadd11ab
artemigla <igla.artem@gmail.com>
2022-03-20 13:20:33 +0200

    Added icons for theme switching.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa';
 import './style.css';
 
 export const Header = ({ title }) => {
+    const [selectTheme, setSelectTheme] = useState(false);
+
+    const clickTheme = () => {
+        setSelectTheme(!selectTheme)
+    }
     return (
         <header className='container'>
+            <div></div>
             <div className='titleHeader'>
                 <h1 className='title'>{title}</h1>
             </div>
+            <button
+                className='selectTheme'
+                type='button'
+                onClick={clickTheme}
+            >
+                {!selectTheme ? <FaSun size={21} color={'white'} /> : <FaMoon size={21} color={'white'} />}
+            </button>
         </header>
-
     );
 }
